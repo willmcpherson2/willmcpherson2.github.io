cab271fd2ddbb90db185a7da7ed72f59a48780f1
William McPherson <willmcpherson2@gmail.com>
2025-03-18 18:47:02 +1100

    Set up GitHub page from template

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "GitHub pages environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          ruby_3_4
+          rubyPackages_3_4.jekyll
+          gnumake
+        ];
+      };
+    };
+}
